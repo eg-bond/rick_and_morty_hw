26d25a74556e7604079ad3e7895217c9bdebe66b
eg-bond <eg-bond@yandex.ru>
2024-05-28 14:27:19 +0300

    dock: basic empty vite-tsx setup

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import typedCssModulesPlugin from 'vite-plugin-typed-css-modules'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), typedCssModulesPlugin()],
+  server: {
+    open: true,
+  },
+})
