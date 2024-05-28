3e4bd3940d500c3ba5205c3e4e90515edde42b19
eg-bond <eg-bond@yandex.ru>
2024-05-28 23:36:06 +0300

    feat: sorting by ASC and DESC added

--- src/pages/ListItems.tsx
+++ src/pages/ListItems.tsx
@@ -1,14 +1,35 @@
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 type ItemT = {
   id: number
   name: string
+  created: string
 }
 
+type SortingTypeT = 'ASC' | 'DESC'
+
 function ListItems({ items }: { items: ItemT[] }) {
+  const [list, setList] = useState(items)
+
+  function sort(type: SortingTypeT) {
+    const sorted = [...items].sort((a, b) => {
+      const dateA = new Date(a.created)
+      const dateB = new Date(b.created)
+      return type === 'ASC' ? dateA - dateB : dateB - dateA
+    })
+    setList(sorted)
+  }
+
+  useEffect(() => {
+    setList(items)
+  }, [items])
+
   return (
     <div>
-      {items.map(item => (
+      <button onClick={() => sort('DESC')}>Сортировать по убыванию</button>
+      <button onClick={() => sort('ASC')}>Сортировать по возрастанию</button>
+      {list.map(item => (
         <Item key={item.id} name={item.name} to={item.id.toString()} />
       ))}
     </div>
