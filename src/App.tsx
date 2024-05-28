5ef9bd4576734ce1a9d69b725dbb190b30c55890
eg-bond <eg-bond@yandex.ru>
2024-05-28 20:50:00 +0300

    feat: raw implementation of episodes page done

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import { Link, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
+import Main from './pages/Main'
 import Episodes from './pages/Episodes'
 import Locations from './pages/Locations'
 import Characters from './pages/Characters'
+import Episode from './pages/Episode'
 
 function App() {
   return (
-    <>
-      <h1>Rick and Morty HW</h1>
+    <div>
       <ul>
         <li>
-          <Link to='/'>Home</Link>
+          <Link to='/'>Main</Link>
         </li>
         <li>
           <Link to='/episodes'>Episodes</Link>
@@ -23,12 +23,15 @@ function App() {
         </li>
       </ul>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/episodes' element={<Episodes />} />
+        <Route path='/' element={<Main />} />
+        <Route path='/episodes'>
+          <Route index element={<Episodes />} />
+          <Route path=':id' element={<Episode />} />
+        </Route>
         <Route path='/locations' element={<Locations />} />
         <Route path='/characters' element={<Characters />} />
       </Routes>
-    </>
+    </div>
   )
 }
 
--- src/pages/Episodes.tsx
+++ src/pages/Episodes.tsx
@@ -1,5 +1,22 @@
+import { Link } from 'react-router-dom'
+import episodes from '../assets/episode.json'
+
+function Item({ name, to }: { name: string; to: string }) {
+  return (
+    <div>
+      <Link to={`${to}`}>{name}</Link>
+    </div>
+  )
+}
+
 function Episodes() {
-  return <div>Episodes</div>
+  return (
+    <div>
+      {episodes.map(item => (
+        <Item name={item.name} to={item.id.toString()} />
+      ))}
+    </div>
+  )
 }
 
 export default Episodes
