5e908cf0e71b490a04dcc88896e17320a387e69f
eg-bond <eg-bond@yandex.ru>
2024-06-29 12:15:47 +0300

    feat: Mantine Navlink used

--- src/components/Navbar/NavItem.tsx
+++ src/components/Navbar/NavItem.tsx
@@ -1,6 +1,6 @@
 import { NavLink } from 'react-router-dom';
 import { AppRoutes } from '@/types/routesTypes';
-import s from './navbar.module.css';
+import { NavLink as MantineNavlink } from '@mantine/core';
 
 interface NavItemProps {
   route: AppRoutes;
@@ -10,12 +10,13 @@ interface NavItemProps {
 export function NavItem({ route, text }: NavItemProps) {
   return (
     <li>
-      <NavLink
-        style={{ textDecoration: 'none' }}
-        className={({ isActive }) => (isActive ? s.red : undefined)}
-        to={`${route}`}>
-        {text}
-      </NavLink>
+      <MantineNavlink
+        variant='filled'
+        fw={'bold'}
+        description={text}
+        to={`${route}`}
+        component={NavLink}
+      />
     </li>
   );
 }
