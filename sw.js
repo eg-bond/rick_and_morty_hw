e97fbe243dd6abb9607925e3322b8f40fc0ab86a
eg-bond <eg-bond@yandex.ru>
2024-07-03 23:59:05 +0300

    feat: static caching part 1

--- sw.js
+++ sw.js
@@ -1,9 +1,28 @@
+const staticCacheName = 'site-static-v1';
+
+const ASSETS = ['/', '/index.html', '/episodes'];
+
 // install event
-self.addEventListener('install', event => {
-  console.log('Service worker installed');
+self.addEventListener('install', async event => {
+  const cache = await caches.open(staticCacheName);
+  await cache.addAll(ASSETS);
 });
 
 // activate event
-self.addEventListener('activate', event => {
-  console.log('Service worker activated');
+self.addEventListener('activate', async event => {
+  const cachesKeysArr = await caches.keys();
+  await Promise.all(
+    cachesKeysArr
+      .filter(key => key !== staticCacheName)
+      .map(key => caches.delete(key))
+  );
+});
+
+// fetch event
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cacheRes => {
+      return cacheRes || fetch(event.request);
+    })
+  );
 });
