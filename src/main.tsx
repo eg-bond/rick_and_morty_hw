6bdd35ee4ce912f542b6384a1bb0d5c93214ba40
eg-bond <eg-bond@yandex.ru>
2024-07-03 12:33:04 +0300

    feat: serviceWorker registered

--- src/main.tsx
+++ src/main.tsx
@@ -7,3 +7,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <App />
   </BrowserRouter>
 );
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js');
+  });
+}
