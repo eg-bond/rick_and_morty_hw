c721f8b6181b9955705b3f40734bb41ace273fda
eg-bond <eg-bond@yandex.ru>
2024-07-18 12:25:28 +0300

    feat: dynamic cache added

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,7 @@
 const staticCacheName = 'site-static-v1';
+const dynamicCacheName = 'site-dynamic-v1';
 
-const ASSETS = ['/', '/index.html', '/episodes'];
+const ASSETS = ['/index.html'];
 
 // install event
 self.addEventListener('install', async event => {
@@ -13,7 +14,7 @@ self.addEventListener('activate', async event => {
   const cachesKeysArr = await caches.keys();
   await Promise.all(
     cachesKeysArr
-      .filter(key => key !== staticCacheName)
+      .filter(key => (key !== staticCacheName) | dynamicCacheName)
       .map(key => caches.delete(key))
   );
 });
@@ -21,8 +22,17 @@ self.addEventListener('activate', async event => {
 // fetch event
 self.addEventListener('fetch', event => {
   event.respondWith(
+    // getting cached data
     caches.match(event.request).then(cacheRes => {
-      return cacheRes || fetch(event.request);
+      return (
+        cacheRes ||
+        fetch(event.request).then(response => {
+          return caches.open(dynamicCacheName).then(cache => {
+            cache.put(event.request.url, response.clone());
+            return response;
+          });
+        })
+      );
     })
   );
 });
